0bb236cec1a7806ca29f09d5c4abf41703bb5843
chickenshifu_local <tobias@Lingnas-MacBook-Pro.local>
2021-02-06 21:59:56 +0100

    login and session activation working

--- credentials.py
+++ credentials.py
@@ -4,11 +4,16 @@
 import os
 
 def credentials():
+    #Please set your WIKIFOLIO username (email) and password via environment variables (<export WIKI_USR='test@test.com'> and <export WIKIFOLIO_PWD='foobar'>)
+
     USR = os.getenv('WIKIFOLIO_USR')
     PWD = os.getenv('WIKIFOLIO_PWD')
 
-    credentials = {'USR': USR, 'PWD': PWD} 
- 
+    #URL to your wikifolio
+    WIKI = 'url'
+
+
+    credentials = {'USR': USR, 'PWD': PWD, 'WIKI': WIKI} 
     return credentials
 
 if __name__ == 'main':
